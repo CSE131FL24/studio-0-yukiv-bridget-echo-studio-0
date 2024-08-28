e22bff84b57e625f11e2ba710fdf43a0ba736d7d
wyqr3 <wyqr3@localhost>
2024-08-28 13:58:22 -0500

    updated

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -5,5 +5,7 @@ public class Message {
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
 		System.out.println("helo");
+		System.out.println("hi");
+		System.out.println("hiii");
 		}
 }
